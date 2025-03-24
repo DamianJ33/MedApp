07bcba6f14804b25a0095ffd1ca08df1f70e7738
Damian Jasiński <dam03jas@gmail.com>
2025-03-24 23:23:31 +0100

    UserDatabaseHelper - funkcja getUser za pomocą ID i za pomocą email

--- app/src/main/java/com/example/mediapp/ui/theme/Screen/UserDatabaseHelper.kt
+++ app/src/main/java/com/example/mediapp/ui/theme/Screen/UserDatabaseHelper.kt
@@ -59,6 +59,52 @@ class UserDatabaseHelper {
     }
 
 
+
+    // Get user by ID
+    fun getUser(userId: Int): User? {
+        val connection = connectToDatabase()
+        if (connection != null) {
+            val query = "SELECT * FROM users WHERE id = ?"
+            val preparedStatement = connection.prepareStatement(query)
+            preparedStatement.setInt(1, userId)
+            val resultSet = preparedStatement.executeQuery()
+            if (resultSet.next()) {
+                val id = resultSet.getInt("id")
+                val email = resultSet.getString("email")
+                val password = resultSet.getString("password")
+                resultSet.close()
+                connection.close()
+                return User(id, email, password)
+            }
+            resultSet.close()
+            connection.close()
+        }
+        return null
+    }
+
+    // Get user by email
+    fun getUserByEmail(email: String): User? {
+        val connection = connectToDatabase()
+        if (connection != null) {
+            val query = "SELECT * FROM users WHERE email = ?"
+            val preparedStatement = connection.prepareStatement(query)
+            preparedStatement.setString(1, email)
+            val resultSet = preparedStatement.executeQuery()
+            if (resultSet.next()) {
+                val id = resultSet.getInt("id")
+                val userEmail = resultSet.getString("email")
+                val password = resultSet.getString("password")
+                resultSet.close()
+                connection.close()
+                return User(id, userEmail, password)
+            }
+            resultSet.close()
+            connection.close()
+        }
+        return null
+    }
+
+
 }
 // User data class to store user information
 data class User(val id: Int, val email: String, val password: String)
