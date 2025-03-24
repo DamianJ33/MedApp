907edfc2c76ae88e98bc2d2973e0e347de769fb8
Damian Jasiński <dam03jas@gmail.com>
2025-03-24 23:26:17 +0100

    MyAppNavigation - trasy do przemieszczania się pomiędzy ekranami aplikacji oraz bottombar

---
+++ app/src/main/java/com/example/mediapp/ui/theme/Screen/MyAppNavigation.kt
@@ -0,0 +1,35 @@
+package com.example.mediapp.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+import com.example.mediapp.ui.theme.AuthViewModel
+
+@Composable
+fun AppNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel, navController: NavHostController) {
+
+
+    NavHost(navController = navController, startDestination = "login", modifier = modifier) {
+        composable("login") { LoginScreen(modifier = modifier, navController = navController, authViewModel = authViewModel) }
+        composable("signup") { SignupScreen(modifier = modifier, navController = navController, authViewModel = authViewModel) }
+        composable("home") { HomeScreen(navController, authViewModel) }
+//        composable("appointments") { AppointmentScreen(navController, authViewModel) }
+//        composable("medical_history") { MedicalHistoryScreen(navController, authViewModel) }
+//        composable("profile") { ProfileScreen(navController, authViewModel) }
+    }
+}
+
+
+data class BottomNavItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: String
+)
+
