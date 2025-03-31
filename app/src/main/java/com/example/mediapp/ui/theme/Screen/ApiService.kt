aff2315874e84b15e7f4a338e3893c5a22c8e7e4
Damian Jasiński <dam03jas@gmail.com>
2025-03-31 23:16:44 +0200

    Utworzono interfejs ApiService z dwoma metodami API:
    GetPatient: Metoda GET umożliwiająca pobranie danych pacjenta na podstawie adresu e-mail. Parametr email jest przekazywany dynamicznie w ścieżce URL.
    
    signup: Metoda POST służąca do rejestracji pacjenta. Przyjmuje obiekt Patient w ciele żądania i zwraca odpowiedź SignupResponse.

---
+++ app/src/main/java/com/example/mediapp/ui/theme/Screen/ApiService.kt
@@ -0,0 +1,23 @@
+package com.example.mediapp.ui.theme.Screen
+
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface ApiService {
+    @GET("/api/patients/{email}")
+    suspend fun GetPatient(@Path("email") email: String): Patient
+    @POST("/api/patients/register")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun signup( @Body patient: Patient): Response<SignupResponse>
+
+}
+
