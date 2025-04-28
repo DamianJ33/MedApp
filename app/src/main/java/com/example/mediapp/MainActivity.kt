81222ad73c1a351d893b70e446fb535dc09d896f
Damian Jasiński <dam03jas@gmail.com>
2025-04-28 17:08:08 +0200

    Włączono dagger hilta w MainActivity przez dodanie adnotacji AndroidEntryPoint

--- app/src/main/java/com/example/mediapp/MainActivity.kt
+++ app/src/main/java/com/example/mediapp/MainActivity.kt
@@ -9,18 +9,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.fragment.app.FragmentActivity
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.mediapp.Screen.LoginScreen
 import com.example.mediapp.Screen.MainScreen
 import com.example.mediapp.ui.theme.AuthViewModel
 import com.example.mediapp.ui.theme.MediAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.jetbrains.exposed.sql.Database
 
-
+@AndroidEntryPoint
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +31,7 @@ class MainActivity : FragmentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val authViewModel: AuthViewModel = viewModel()
+            val authViewModel = hiltViewModel<AuthViewModel>()
             // Zainicjalizuj AuthViewModel z kontekstem
             val context = LocalContext.current // Uzyskaj dostęp do contextu w Compose
             authViewModel.init(context)
