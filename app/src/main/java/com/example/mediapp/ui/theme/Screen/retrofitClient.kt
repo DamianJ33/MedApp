0be2cb7f03efe6cfdfbc03148414b3d9043842d9
Damian Jasiński <dam03jas@gmail.com>
2025-04-28 17:09:51 +0200

    Komunikacja aplikacji z serwerem backend przy użyciu retrofita

--- app/src/main/java/com/example/mediapp/ui/theme/Screen/retrofitClient.kt
+++ app/src/main/java/com/example/mediapp/ui/theme/Screen/retrofitClient.kt
@@ -8,7 +8,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.create
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitClient {
     private const val BASE_URL = "http://10.0.2.2:8080"
 
@@ -27,12 +35,14 @@ object RetrofitClient {
         .add(KotlinJsonAdapterFactory()) // ✅ Poprawione
         .build()
 
-    val apiService: ApiService by lazy {
-        Retrofit.Builder()
+    @Provides
+    @Singleton
+    fun apiService(): ApiService{
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(okHttpClient)
             .build()
-            .create(ApiService::class.java)
+            .create()
     }
 }
